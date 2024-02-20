91cf5b1c79e91dd82fa33703db076d39d38f17df
nikita <nikitagawande006@gmail.com>
2024-02-20 15:26:48 +0530

    delete and add function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+ var count=1
 function App() {
+  
+  var [todo, setTodo] = useState([])
+  const addTodo = () =>
+  {
+    
+    console.log("Add todo")
+     const todoText =document.getElementById("todoInput").value
+     let todoObject =
+    {
+      id:count++,
+      title:todoText
+    }
+     console.log("todoText :"+todoText)
+     todo.push(todoObject)
+     setTodo([...todo])// "..."spread operator in javascript setTodo 
+    
+
+  }  
+  
+  const deleteTodo =(id) => 
+  {
+    console.log("Delete todo id:",id)
+    todo =todo.filter(tempTodo =>
+      {
+        if(id === tempTodo.id)
+        {
+          return false
+        }
+        else{
+          return true
+        }
+
+      })
+      setTodo([...todo])
+  }
+  //console.log("Length of todo : ",todo.length)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <h1>Todo App</h1>
+      <input id="todoInput" type='text' placeholder='Add your todo here....'/>
+      <button onClick={addTodo}>Add</button>
+      {todo.map(tempTodo =>
+        {
+          return <div>{tempTodo.title}
+          <button onClick={()=>deleteTodo(tempTodo.id)}>Delete</button>
+          </div>
+        })
+      }     
+   </div>
   );
 }
-
 export default App;
