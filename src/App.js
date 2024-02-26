19f812c8d648ceca3deaf083037c832f64159e3a
nikita <nikitagawande006@gmail.com>
2024-02-27 00:12:02 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,57 +1,129 @@
+import './App.css'
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
- var count=1
-function App() {
-  
-  var [todo, setTodo] = useState([])
-  const addTodo = () =>
-  {
-    
-    console.log("Add todo")
-     const todoText =document.getElementById("todoInput").value
-     let todoObject =
+import TodoListItem from './TodoListItem';
+
+var count = 1
+
+function App() 
+{
+  //to handle action in js
+  const [editingFlag,seteditingFlag] = useState(-1)
+  var [todo, setTodo] = useState([
+    {
+    id:count++,
+    title:"todo1",
+    completed:false
+    },
     {
       id:count++,
-      title:todoText
+    title:"todo2",
+    completed:false
+    },
+    {
+    id:count++,
+    title:"todo3",
+    completed:true
     }
-     console.log("todoText :"+todoText)
-     todo.push(todoObject)
-     setTodo([...todo])// "..."spread operator in javascript setTodo 
-    
 
-  }  
-  
-  const deleteTodo =(id) => 
+  ])  //using use state hooks of react
+  const addTodo=()=>
   {
-    console.log("Delete todo id:",id)
-    todo =todo.filter(tempTodo =>
-      {
-        if(id === tempTodo.id)
-        {
-          return false
-        }
-        else{
-          return true
-        }
+    console.log("add todo")
+    const todoText = document.getElementById("todoInput").value
+    let todoObject = {
+                         id:count++, //careating an object 
+                         title:todoText,
+                         completed:false
+                      }
+    console.log("todoText: "+todoText)
+    todo.push(todoObject)
+    setTodo([...todo]) //spred operator to open or expand the array
+  }
+  
+  console.log("length of todo :"+todo.length)
 
+  const deleteTodo=(id)=>
+  {
+    todo = todo.filter(tempTodo=>
+      {
+         if(id===tempTodo.id)
+         {
+          return false
+         }
+         else
+         {
+           return true
+         }
       })
       setTodo([...todo])
   }
-  //console.log("Length of todo : ",todo.length)
-  return (
-   <div>
-      <h1>Todo App</h1>
-      <input id="todoInput" type='text' placeholder='Add your todo here....'/>
-      <button onClick={addTodo}>Add</button>
-      {todo.map(tempTodo =>
+  const checkedChange=(id)=>
+  {
+    console.log("id:",id)
+    todo.map(todoTemp=>
+      {
+        if(id===todoTemp.id)
         {
-          return <div>{tempTodo.title}
-          <button onClick={()=>deleteTodo(tempTodo.id)}>Delete</button>
-          </div>
+            todoTemp.completed = !todoTemp.completed
+        }
+        return todoTemp
+      })
+      setTodo([...todo])
+  }
+  const editTodo=(id)=>
+  {
+    console.log(" editTodo")
+    seteditingFlag(id)
+  }
+  const saveEditedTodo =(id) =>
+  {
+    console.log("saveEditedTodo")
+    let updatedTodoText = document.getElementById("editingTodo").value
+    todo =todo.map(todoTemp =>
+      {
+        if(todoTemp.id === id)
+        {
+          todoTemp.title = updatedTodoText
+        }
+        return todoTemp
+      })
+      setTodo([...todo])
+      seteditingFlag(-1)
+  }
+ /* const divStyle = {
+    backgroundColor:"pink",
+    margin:"30px",
+    padding:"10px"
+  }*/
+//to show in frontend
+  return (
+    <div id='parentDiv'>
+      <h1 className='header'>Todo App</h1>
+      <input id="todoInput" type="text" placeholder='add todo...'/>
+      <button onClick={addTodo}>add</button> <br/>
+      {
+      todo.map(tempTodo =>
+        {
+          return <TodoListItem
+          tempTodo = {tempTodo}
+          checkedChange ={checkedChange}
+          editingFlag ={editingFlag}        
+          saveEditedTodo ={saveEditedTodo}
+          deleteTodo={deleteTodo}
+          editTodo ={editTodo}
+          />   
         })
-      }     
-   </div>
+       }
+    </div>
   );
 }
-export default App;
+export default App;
+/*
+tempTodo
+checkedChange
+editingFlag
+saveEditedTodo
+deleteTodo
+editTodo*/ 
---
+++ src/TodoListItem.js
@@ -0,0 +1,49 @@
+//reuseable component
+const TodoListItem =(props)=>{
+    return <div key={props.tempTodo.id}>
+    {
+    props.tempTodo.completed===true ?       // using turney operator ( condition ? if execurtion : else execution)
+    <><input type='checkbox'  onChange={()=>props.checkedChange(props.tempTodo.id)} checked/> <s>{props.tempTodo.title}</s></> :
+    <><input type='checkbox' onChange={()=>props.checkedChange(props.tempTodo.id)}/>
+    {
+      props.editingFlag === props.tempTodo.id ?
+      <>
+       <input type='text' id='editingTodo'defaultValue={props.tempTodo.title}/>
+       <button onClick={()=>props.deleteTodo(props.tempTodo.id)}>delete</button>
+       <button onClick={()=>props.saveEditedTodo(props.tempTodo.id)}> save</button>
+      </> :
+      <>
+      {props.tempTodo.title}
+      <button onClick={()=>props.deleteTodo(props.tempTodo.id)}>delete</button>
+      <button onClick={()=>props.editTodo(props.tempTodo.id)}>Edit</button>
+      </>
+    }
+    </>
+    } 
+   
+    </div>
+
+}
+
+export default TodoListItem;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
